74bbd43b7a101c9f42e67c452dbc6c569e268552
rkuc <rkuc@ivmx.pl>
2017-05-04 15:42:52 +0100

    Initial commit cd

--- src/main/java/pl/raselek/example/maven/Main.java
+++ src/main/java/pl/raselek/example/maven/Main.java
@@ -2,9 +2,15 @@ package pl.raselek.example.maven;
 
 public class Main {
 	
+	@Deprecated
+	public void fun(){
+		
+	}
+	
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
-	}
+		new Main().fun();
+	}	
 
 }
